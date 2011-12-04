cf5372fd8c6b16994ce082a2ecbbf1ea9681d39f
Alexey.Nesterov <Alexey.Nesterov@live.com>
2011-12-04 12:27:16 +0400

    hotkey for goto next diff, highlight mode

--- changetracker.py
+++ changetracker.py
@@ -8,6 +8,10 @@ from sublime import Region
 from threading import Timer
 
 class HighlightChangesCore:
+    # highlight modes
+    MODE_DOTS = "dots"
+    MODE_TEXT = "text"
+
     def __init__(self):
         self.settings = sublime.load_settings(__name__ + '.sublime-settings')
         self.regions = []
@@ -20,10 +24,14 @@ class HighlightChangesCore:
     def highlight_delay(self):
         return self.settings.get("highlight_delay", 2000) # default delay is 2000ms
 
+    def highlight_mode(self):
+        return self.settings.get("highlight_mode", HighlightChangesCore.MODE_DOTS) # default mode is "dots"
+
     def goto_next_diff(self, view):
         if self._current_region >= len(self.regions):
-            self._current_region = 0
-        view.show(self.regions[self._current_region])
+            self._current_region = 0        
+        if len(self.regions) > 0:
+            view.show(self.regions[self._current_region])
         self._current_region += 1
 
     def get_diff(self, s1, s2):   
@@ -41,7 +49,10 @@ class HighlightChangesCore:
             currentText = view.substr(Region(0, view.size()))        
             originalText = codecs.open(view.file_name(), "r", "utf-8").read()
             diffs = self.get_diff(originalText, currentText)
-            self.regions = [Region(d[0], d[1]) for d in diffs if d[0] != d[1]]
+            if self.highlight_mode() == HighlightChangesCore.MODE_TEXT:
+                self.regions = [Region(d[0], d[1]) for d in diffs if d[0] != d[1]]
+            if self.highlight_mode() == HighlightChangesCore.MODE_DOTS:
+                self.regions = [Region(d[0], d[0]) for d in diffs if d[0] != d[1]]                
             view.add_regions("changes", self.regions, self._scope, "dot")
         except:
             pass
@@ -58,7 +69,7 @@ class HighlightchangesCommand(sublime_plugin.TextCommand):
         self.highlightCore.highlight(self.view)
 
 class GotonextdiffCommand(HighlightchangesCommand):    
-    def run(self, edit): 
+    def run(self, edit):
         self.highlightCore.highlight(self.view)
         self.highlightCore.goto_next_diff(self.view)
 
